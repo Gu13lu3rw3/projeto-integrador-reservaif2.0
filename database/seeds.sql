e88e74b2b680f84a7857257607908f6302775dbf
Gu13lu3rw3 <catce.2025111mtds0018@aluno.ifpi.edu.br>
2026-04-25 14:34:59 -0300

    Update seeds.sql

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO HORARIO_OFICIAL (id_sala, dia_semana, hora_inicio, hora_fim, descricao) VALUES 
+(1, 'Segunda-feira', '07:30:00', '09:10:00', 'Engenharia de Software (TDS)'),
+(1, 'Segunda-feira', '09:20:00', '11:00:00', 'Programação back-end (TDS)'),
+(2, 'Segunda-feira', '13:30:00', '15:10:00', 'Programação Orientada a Objetos (TDS)');
