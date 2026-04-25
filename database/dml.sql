4f6cf9f0a76ebe6b3b9a18dc21d33bc56a7296ef
Gu13lu3rw3 <catce.2025111mtds0018@aluno.ifpi.edu.br>
2026-04-25 14:30:48 -0300

    Update dml.sql

--- database/dml.sql
+++ database/dml.sql
@@ -1,1 +1,15 @@
+INSERT INTO USUARIO (nome, email, senha, perfil_acesso) VALUES 
+('Guilherme Silva', 'guilherme@ifpi.edu.br', '$2b$12$eImiTXuWVxjM72fGC3W/m.', 'Admin'),
+('Carlos Eduardo', 'carlos@ifpi.edu.br', '$2b$12$eImiTXuWVxjM72fGC3W/m.', 'Coordenador'),
+('Antônio Marcos', 'antonio@ifpi.edu.br', '$2b$12$eImiTXuWVxjM72fGC3W/m.', 'Professor'),
+('Equipe Manutenção', 'manutencao@ifpi.edu.br', '$2b$12$eImiTXuWVxjM72fGC3W/m.', 'Manutenção');
 
+INSERT INTO SALA (nome, capacidade, localizacao, status_manutencao) VALUES 
+('Sala B2-01', 40, 'Bloco B - Piso 2', 'Disponível'),
+('Laboratório TDS', 30, 'Bloco A - Piso 1', 'Disponível'),
+('Auditório Central', 150, 'Bloco C', 'Disponível');
+
+INSERT INTO EQUIPAMENTO (nome, descricao, id_sala) VALUES 
+('Projetor Epson', 'Modelo X41+', 1),
+('Ar Condicionado', 'Split 18000 BTUs', 1),
+('Projetor BenQ', 'Modelo MW535', 2);
