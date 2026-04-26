cd2ebb7c0717a677ba721ba29b90aa023a122f47
Gu13lu3rw3 <catce.2025111mtds0018@aluno.ifpi.edu.br>
2026-04-26 15:43:53 -0300

    Update problema.py

--- backend/models/problema.py
+++ backend/models/problema.py
@@ -1,18 +1,1 @@
-from datetime import datetime
 
-class Problema:
-    def __init__(self, id_sala, descricao, id_professor_reporte):
-        self.id_sala = id_sala
-        self.descricao = descricao
-        self.id_professor_reporte = id_professor_reporte
-        self.data_reporte = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.status = "Pendente"
-
-    def to_dict(self):
-        return {
-            "id_sala": self.id_sala,
-            "descricao": self.descricao,
-            "id_professor_reporte": self.id_professor_reporte,
-            "data_reporte": self.data_reporte,
-            "status": self.status
-        }
