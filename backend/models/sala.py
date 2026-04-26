0dc3d90952d844fd77c555172942fae218ec7e6e
Gu13lu3rw3 <catce.2025111mtds0018@aluno.ifpi.edu.br>
2026-04-26 15:44:12 -0300

    Update sala.py

--- backend/models/sala.py
+++ backend/models/sala.py
@@ -1,14 +1,1 @@
-class Sala:
-    def __init__(self, nome, capacidade, localizacao):
-        self.nome = nome
-        self.capacidade = capacidade
-        self.localizacao = localizacao
-        self.status_manutencao = "Disponível"
 
-    def to_dict(self):
-        return {
-            "nome": self.nome,
-            "capacidade": self.capacidade,
-            "localizacao": self.localizacao,
-            "status_manutencao": self.status_manutencao
-        }
