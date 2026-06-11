707a9e03be2db55ccc343054723adfe930a7e3d2
Gu13lu3rw3 <catce.2025111mtds0018@aluno.ifpi.edu.br>
2026-06-11 12:30:04 -0300

    Update sala_controller.py

--- backend/controllers/sala_controller.py
+++ backend/controllers/sala_controller.py
@@ -1,23 +1,31 @@
 from flask import render_template, request, redirect
 from models.dados import salas
+from controllers.auth_controller import login_required, roles_required
 
+@login_required
 def listar_salas():
     return render_template('listar_salas.html', salas=salas)
 
+@login_required
+@roles_required('Administrador')
 def form_cadastrar_sala():
     return render_template('cadastrar_salas.html')
 
+@login_required
+@roles_required('Administrador')
 def salvar_sala():
     nome = request.form.get('nome')
     capacidade = request.form.get('capacidade')
     localizacao = request.form.get('localizacao')
+    equipamentos = request.form.get('equipamentos')
     
     if nome and capacidade:
         nova_sala = {
             "id": len(salas) + 1,
             "nome": nome, 
             "capacidade": capacidade,
-            "localizacao": localizacao
+            "localizacao": localizacao,
+            "equipamentos": equipamentos if equipamentos else "Nenhum"
         }
         salas.append(nova_sala)
         
